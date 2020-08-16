3d4c8474f38ae6be2077e02d4d2a8e47e26a6a48
rohini272 <65247301+rohini272@users.noreply.github.com>
2020-08-16 16:43:48 +0530

    Add files via upload

---
+++ 2.js
@@ -0,0 +1,11 @@
+function username() {
+    let username.Element = document.querySelector()"#userid";
+    username.Element.innerHtml = username;
+}
+function password() {
+    let.Elen = document.querySelector()id";
+    username.Element.innerHtml = username;
+
+
+
+}
